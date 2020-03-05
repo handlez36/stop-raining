b80f168c8c55f7809b9fd229d173a2b322eaec94
Brandon McLean <brandonmclean@live.com>
2020-03-05 00:41:32 -0500

    Move Open Cage Api key to an environment variable

---
+++ src/components/MiniForecast.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./MiniForecast.scss";
+
+const MiniForecast = ({ forecast }) => {
+  return <div className='mini-forecast'></div>;
+};
+
+export default MiniForecast;
